ab8aac3a7ea5b6aec51e50d6ef991d10474a7160
jannessantoso <jannessantoso@gmail.com>
2023-10-31 12:59:04 +0700

    M

--- course_object_detection/module-2/detect-object-yolov8/4. Object Counting.py
+++ course_object_detection/module-2/detect-object-yolov8/4. Object Counting.py
@@ -13,24 +13,33 @@ cap = cv2.VideoCapture("../../../../data/highway_traffic.mp4")
 
 # 3. Load tracker
 mot = MultiObjectTracking()
-tracker = mot.ocsort()
+tracker = mot.ocsort(min_hits=5, #min_hits mean ID only given when frame detected minimum 5 frame in a row
+                      max_age=10, #max_age mean if existing detection gone in 10 frame then new ID given
+                      iou_threshold=0.3)
 
 # Object Counting Area
-crossing_area_1 = np.array([(682, 372), (947, 369), (1264, 503), (744, 576)])
-crossing_area_2 = np.array([(378, 355), (599, 355), (544, 562), (93, 484)])
+crossing_area_1 = np.array([(343, 22), (584,13), (632, 42), (624, 185), (313, 121)])
+crossing_area_2 = np.array([(12, 183), (6, 25), (290, 23), (280, 188)])
 vehicles_ids1 = set()
 vehicles_ids2 = set()
+
+x_start, y_start, x_end, y_end = 1, 164, 637, 356
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
+
+    cropped_frame = frame[y_start:y_end, x_start:x_end]
+    cv2.putText(frame, "Area Start Detect", (x_start, y_start - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0),1)
+    cv2.rectangle(frame, (x_start, y_start),(x_end, y_end), (0,255,0), 1)
+
     # 1. We detect the objects
-    bboxes, class_ids, scores = od.detect(frame, imgsz=640, conf=0.25, device="mps")
+    bboxes, class_ids, scores = od.detect(cropped_frame, imgsz=640, conf=0.25, device="mps")
 
     # 2. We track the objects detected
-    bboxes_ids = tracker.update(bboxes, scores, class_ids, frame)
+    bboxes_ids = tracker.update(bboxes, scores, class_ids, cropped_frame)
     for bbox_id in bboxes_ids:
         (x, y, x2, y2, object_id, class_id, score) = np.array(bbox_id)
         cx = int((x + x2) / 2)
@@ -38,10 +47,10 @@ while True:
 
         # draw bounding box
         # cv2.rectangle(frame, (x, y), (x2, y2), od.colors[class_id], 2)
-        cv2.circle(frame, (cx, cy), 5, od.colors[class_id], -1)
+        cv2.circle(cropped_frame, (cx, cy), 5, od.colors[class_id], -1)
 
         # Object id
-        cv2.putText(frame, "{}".format(object_id), (cx, cy - 10), cv2.FONT_HERSHEY_PLAIN, 1.4, od.colors[class_id], 2)
+        cv2.putText(cropped_frame, "{}".format(object_id), (cx, cy - 10), cv2.FONT_HERSHEY_PLAIN, 1.4, od.colors[class_id], 2)
 
         is_inside1 = cv2.pointPolygonTest(crossing_area_1, (cx, cy), False)
         is_inside2 = cv2.pointPolygonTest(crossing_area_2, (cx, cy), False)
@@ -53,17 +62,18 @@ while True:
 
 
 
-    cv2.putText(frame, "VEHICLES AREA 1: {}".format(len(vehicles_ids1)), (600, 50), cv2.FONT_HERSHEY_PLAIN,
-                1.5, (15, 225, 215), 2)
+    cv2.putText(frame, "VEHICLES AREA 1: {}".format(len(vehicles_ids1)), (400, 50), cv2.FONT_HERSHEY_PLAIN,
+                1, (15, 225, 215), 1)
     cv2.putText(frame, "VEHICLES AREA 2: {}".format(len(vehicles_ids2)), (10, 50), cv2.FONT_HERSHEY_PLAIN,
-                1.5, (15, 225, 215), 2)
+                1, (15, 225, 215), 1)
 
     # Draw area
-    cv2.polylines(frame, [crossing_area_1], True, (15, 225, 215), 2)
-    cv2.polylines(frame, [crossing_area_2], True, (15, 225, 215), 2)
+    cv2.polylines(cropped_frame, [crossing_area_1], True, (15, 225, 215), 2)
+    cv2.polylines(cropped_frame, [crossing_area_2], True, (0, 0, 255), 2)
 
     cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1)
+    cv2.imshow("Cropped Frame", cropped_frame)
+    key = cv2.waitKey(0)
     if key == 27:
         break
 
--- course_object_detection/module-2/detect-object-yolov8/extras/print_coordinates_onclick.py
+++ course_object_detection/module-2/detect-object-yolov8/extras/print_coordinates_onclick.py
@@ -1,6 +1,7 @@
 import cv2
 
-cap = cv2.VideoCapture("../demo/highway.mp4")
+# cap = cv2.VideoCapture("../demo/highway.mp4")
+cap = cv2.VideoCapture("../../../../../data/highway_traffic.mp4")
 
 def print_coordinates(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -8,6 +9,9 @@ def print_coordinates(event, x, y, flags, params):
 
 cv2.namedWindow("Frame")
 cv2.setMouseCallback("Frame", print_coordinates)
+
+x_start, y_start, x_end, y_end = 1, 164, 637, 356
+
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -15,11 +19,12 @@ while True:
         # set frame to 0
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
         continue
+    cropped_frame = frame[y_start:y_end, x_start:x_end]
 
 
 
-
-    cv2.imshow("Frame", frame)
+    # cv2.imshow("Frame", frame)
+    cv2.imshow("Frame", cropped_frame)
 
     key = cv2.waitKey(30)
     if key == 27:
